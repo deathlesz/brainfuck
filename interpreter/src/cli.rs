03badd5b57815ac103b48e653c2bf187087f9ac1
Deathlesz <deathless.mcd@gmail.com>
2024-10-22 09:44:18 +0300

    fix(interpreter,jit): remove output and change about

--- interpreter/src/cli.rs
+++ interpreter/src/cli.rs
@@ -1,10 +1,8 @@
 use std::path::PathBuf;
 
 #[derive(Debug, Clone, clap::Parser)]
-#[command(version, about = "Compiles brainfuck into object files/LLVM IR.")]
+#[command(version, about = "Runs brainfuck using interpreter.")]
 pub struct Cli {
     #[arg(help = "Path to file with source code")]
     pub source: Option<PathBuf>,
-    #[arg(short, long, help = "Path to output file")]
-    pub output: Option<PathBuf>,
 }
