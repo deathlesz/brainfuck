04ce220073e2bf6255a6f6bc31b327db1c9adfcf
Deathlesz <deathless.mcd@gmail.com>
2024-11-06 23:05:39 +0300

    test(parser): add some tests for instruction display

--- parser/src/instruction.rs
+++ parser/src/instruction.rs
@@ -45,9 +45,9 @@ impl Display for Instruction {
                 write!(
                     f,
                     "[-{}{}{}]",
-                    symbol.repeat(offset as usize),
+                    symbol.repeat(offset.unsigned_abs()),
                     add.repeat((mult as i8).unsigned_abs() as usize),
-                    symbol_opposite.repeat(offset as usize),
+                    symbol_opposite.repeat(offset.unsigned_abs()),
                 )
             }
             MoveUntilZero(count) => {
@@ -57,4 +57,54 @@ impl Display for Instruction {
             }
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Instruction::*;
+
+    macro_rules! test {
+        ($name:ident,$inst:expr,$result:expr) => {
+            #[test]
+            fn $name() {
+                assert_eq!($inst.to_string(), $result)
+            }
+        };
+    }
+
+    test!(display_add_positive, Add(10), "++++++++++");
+    test!(display_add_negative, Add(5u8.wrapping_neg()), "-----");
+    test!(display_move_positive, Move(12), ">>>>>>>>>>>>");
+    test!(display_move_negative, Move(-7), "<<<<<<<");
+    test!(display_in, In, ",");
+    test!(display_out, Out, ".");
+    test!(display_jz, JumpIfZero(81), "[");
+    test!(display_jne, JumpIfNotZero(1), "]");
+    test!(display_clear, Clear, "[-]");
+    test!(
+        display_multiply_positive_positive,
+        Multiply(5, 5),
+        "[->>>>>+++++<<<<<]"
+    );
+    test!(
+        display_multiply_positive_negative,
+        Multiply(2, 3u8.wrapping_neg()),
+        "[->>---<<]"
+    );
+    test!(
+        display_multiply_negative_positive,
+        Multiply(-7, 2),
+        "[-<<<<<<<++>>>>>>>]"
+    );
+    test!(
+        display_multiply_negative_negative,
+        Multiply(-1, 4u8.wrapping_neg()),
+        "[-<---->]"
+    );
+    test!(display_move_until_zero_positive, MoveUntilZero(3), "[>>>]");
+    test!(
+        display_move_until_zero_negative,
+        MoveUntilZero(-9),
+        "[<<<<<<<<<]"
+    );
 }
